d95803f6f7b074e54c0c0220878c751430497a27
Kyle <kylereaves@gmail.com>
2021-08-05 14:31:51 -0400

    100 geocoded errors in Paterson corrected

---
+++ Paterson/paterson_repl_dict.py
@@ -0,0 +1,16 @@
+paterson_repl_dict = {
+    " (N 2ND ST)": '',
+    "PT ": "",
+    "PT.": "",
+    "FT.": "",
+    "FRT": "",
+    "FRTS. ": "",
+    "RRS": "",
+    "FRONT": "",
+    "REAR": "",
+    "TR CV": "TR",
+    "MC BRIDE": "MCBRIDE",
+    "BOULDER RUN": "BOULDER RUN ROAD",
+    "FT": "",
+    "MILL PLOT ": ""
+}
