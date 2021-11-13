5d9fae8debb0f5e1729ed39a8ce4ef36d1d3b8f3
Kyle <kylereaves@gmail.com>
2021-11-13 00:04:32 -0500

    Deleted unnecessary pickling and markdown.

---
+++ schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE jersey_city (
+    property_id int,
+    street_address text,
+    owner_name text,
+    owners_mailing_address text,
+    city_state_zip text,
+    full_address text,
+    number_properties_owned int,
+    list_properties_owned text[],
+    latitude float,
+    longitude float
+);
+
