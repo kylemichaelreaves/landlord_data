f096813a8594130f53e7e6c25460dec80b2cf259
Kyle <kylereaves@gmail.com>
2021-11-13 21:49:46 -0500

    Fixed asymmetry between dataframe and pg schema

--- schema.sql
+++ schema.sql
@@ -4,9 +4,11 @@ CREATE TABLE jersey_city (
     owner_name text,
     owners_mailing_address text,
     city_state_zip text,
-    full_address text,
-    number_properties_owned int,
+    property_full_address text,
+    units int,
     list_properties_owned text[],
+    number_properties_owned int,
+    g_code text,
     latitude numeric(8, 6),
     longitude numeric(8, 6),
     primary key (id)
